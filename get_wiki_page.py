16c4d99f59dfe33ed8edfe15db9bd295018efe2e
Oscar Zwagers <oscarzwagers@MacBook-Pro-van-Oscar.local>
2021-03-09 14:24:38 +0100

    comments and docstrings added to python script

--- get_wiki_page.py
+++ get_wiki_page.py
@@ -2,6 +2,12 @@ import urllib.request, re
 
 
 def get_page(url):
+    """
+    This function will get the text from wikipedia and clean the file
+    from unnecesary html tags and comments. Also text not part of the
+    content is removed. This will be stored in a file called
+    "wikipedia_page.txt"
+    """
     response = urllib.request.urlopen(url)
     html = response.read().decode('utf-8').replace("\n", "qqqqq")
     pattern = re.compile('<div id="content".*')
@@ -20,6 +26,7 @@ def get_page(url):
 
 def main():
     get_page("https://nl.wikipedia.org/wiki/Rijksuniversiteit_Groningen")
+    # the link of the most recent version of the wikipedia page
 
 
 if __name__ == "__main__":
